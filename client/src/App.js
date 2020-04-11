aec1cb930e230fa114c3a815fed597b9055f7f4d
Sangeetha Kanna <sangeetha.kanna@hcl.com>
2020-04-11 15:38:03 +0530

    Adding client

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import NavBar from './components/Navbar.js';
+import Home from './components/Home.js';
+
+import './App.css';
+
+function App() {
+    return (
+        <div className='App'>
+            <NavBar />
+            <Home />
+        </div>
+    )
+}
+
+export default App;
---
+++ client/src/components/layout/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Home = () => {
+
+    return (
+        <section className="home">
+            <div className="darkOverlay">
+                <div className="home-inner">
+                    <h1 className="h1">Contacts</h1>
+                    <div className="buttons">
+                        
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Home;
